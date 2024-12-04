52e2740182a3c98880e7eb49ab9dbfcffca33c74
MATUSHIMA02 <andrew.matushima.silva@escola.pr.gov.br>
2024-12-04 20:27:14 -0300

    Create moveis.js

---
+++ moveis.js
@@ -0,0 +1,297 @@
+}
+{
+title: "",Os incríveis 2 (2018)
+genre: "",:Ação 
+ageRating: L,
+synopsis: "A sequência da família de super-heróis, cheia de ação e cenas dinâmicas, mas com um tom leve e adequado para crianças
+",
+trailer: "https://youtu.be/8NURuV5GmPY?feature=shared
+"
+}
+{
+title: "",Jumanji: Bem-vindo à Selva (2017)
+genre: "",ação 
+ageRating: 10,
+synopsis: "",Um grupo de adolescentes é transportado para dentro de um videogame e precisa enfrentar desafios cheios de ação para sobreviver. Aventura divertida e com ação 
+leve.
+trailer: ""https://youtu.be/tnpV3q8Q3gg?si=2tLEnQg97zXKlfzO
+}
+{
+title: "",Homem-Aranha: De Volta ao Lar (2017)
+genre: "",ação 
+ageRating: 12,
+synopsis: "",Peter Parker, equilibrando a vida escolar com sua vida de super-herói, enfrenta grandes desafios em cenas de ação emocionantes, mas com uma classificação moderada.
+trailer: ""https://youtu.be/2x-2iYxgMFU?si=iThqSR1Nmf6ypUX0
+}
+{
+title: "",Pantera Negra (2018)
+genre: "",ação 
+ageRating: 14,
+synopsis: "",Filme do universo Marvel, que conta a história de T’Challa, o Pantera Negra. O filme tem ação bem coreografada, mas é adequado para essa faixa etária.
+trailer: ""https://youtu.be/5d34xP5-9Aw?si=qQegXUg5IwH_DezT
+}
+{
+title: "",John Wick (2014)
+genre: "",ação 
+ageRating: 16,
+synopsis: "",Filme conhecido por suas sequências de luta e ação, com um toque de violência mais estilizada. Ideal para essa faixa etária
+trailer: ""https://youtu.be/C0BMx-qxsP4?si=a-1kHoL6RdBDDYBw
+}
+{
+title: "",Toy Story 3 (2010)
+genre: "",comédia 
+ageRating: L,
+synopsis: ",Os brinquedos de Andy são acidentalmente enviados para uma creche antes de ele ir para a faculdade. Lá, eles enfrentam desafios liderados pelo ursinho Lotso, que controla o local com mãos de ferro. Woody tenta resgatá-los, e juntos elaboram um plano de fuga. Após uma série de perigos, eles escapam e retornam a Andy. No final, Andy doa seus brinquedos para uma menina chamada Bonnie, garantindo que continuem a ser amados.
+trailer: "https://youtu.be/2BlMNH1QTeE?feature=shared
+"
+}
+{
+title: "",Até que a sorte nos separe (2012)
+genre: "",comédia
+ageRating: 10,
+synopsis: "",Depois de gastar todo o prêmio que ganhou na loteria, um homem tenta esconder sua falência da esposa grávida com a ajuda de seu melhor amigo.
+trailer: ""https://youtu.be/dAnzuTb-A8k?si=jrDaSjWOkQXuNiNY
+}
+{
+title: "",Esposa de Mentirinha( 2011)
+genre: "",comédia
+ageRating: 12,
+synopsis: "",Esposa de Mentirinha( 2011)
+ sinopse: com Adam Sandler e Jennifer Aniston. Danny finge estar casado para conquistar mulheres, mas ao se apaixonar por Palmer, inventa que está se divorciando. Para sustentar a mentira, ele pede que sua assistente Katherine finja ser sua esposa. A farsa se complica durante uma viagem ao Havaí, onde todos enfrentam situações engraçadas que fazem Danny repensar sua vida amorosa.
+trailer: ""https://youtu.be/QmL1mdzS544?si=z3umzN29OJZo-lBx
+}
+{
+title: "",Um espião e meio (2016) 
+genre: "",comédia
+ageRating: 14,
+synopsis: "",Um agente da CIA vai atrás de um amigo da época da escola em busca de ajuda para resolver um caso supersecreto. 
+trailer: ""https://youtu.be/Q_Zfna_MSBs?si=y124MoXP2VVR9ByF
+}
+{
+title: "",Deadpool (2016)
+genre: "",comédia
+ageRating: 16,
+synopsis: "",Um mercenário desbocado com poderes de cura busca vingança contra aqueles que destruíram sua vida (com muita comédia ácida)
+trailer: ""https://youtu.be/Q9X-bAE8KTc?si=k1F3oVTVrzsWFOga
+}
+{
+title: "",Divertida Mente (2015)
+genre: "",drama 
+ageRating: L,
+synopsis: "Animação que explora as emoções de uma menina ao lidar com uma grande mudança em sua vida. Uma abordagem emocionante e sensível para todas as idades
+",
+trailer: "https://youtu.be/ukQeR3zYncw?si=f0K7YrPHKM0lvRqU’’
+}
+{
+title: "",Extraordinário (2017)
+genre: "",drama 
+ageRating: 10,
+synopsis: "",Conta a história de um menino com uma deformidade facial que vai para a escola pela primeira vez. Traz temas de aceitação e amizade, com uma abordagem acessível para crianças.
+trailer: ""https://youtu.be/7lyJtY_TPvk?si=-lLaaJCEGM2mwtL1
+}
+{
+title: "",O Menino do Pijama Listrado (2008)
+genre: "",drama 
+ageRating: 12,
+synopsis: "",Conta a história de uma improvável amizade entre um garoto alemão e um menino judeu em um campo de concentração. Temas de guerra e amizade tornam este filme mais profundo.
+trailer: ""https://youtu.be/5Rz5ELTxG48?si=s8WagYXXnobBXqFG
+}
+{
+title: "",A sun (2019)
+genre: "",drama 
+ageRating: 14,
+synopsis: "",A vida de uma família é transformada depois da prisão do filho mais novo e de uma tragédia devastadora.
+trailer: ""https://youtu.be/0bQXpreXRyc?si=7uk9XSxdTehUFsF3
+}
+{
+title: "",Parasita (2019)
+genre: "",drama 
+ageRating: 16,
+synopsis: "",A história acompanha a família Kim, que vive em condições precárias em um porão. Eles se envolvem em um plano engenhoso para se infiltrar na casa da rica família Park, enganando-os para conseguir empregos como tutor, governanta, motorista e outros. À medida que os Kim se estabelecem na casa dos Park, a narrativa revela segredos e tensões que culminam em uma reviravolta surpreendente.
+trailer:""https://youtu.be/xFCrQy5IXI0?si=zz_g0iJi3XhRaEpe
+}
+{
+title: "",Procurando Nemo (2003)
+genre: "",:animação 
+ageRating: L,
+synopsis: "A emocionante jornada de um peixe-palhaço à procura de seu filho perdido. O filme tem mensagens de coragem e perseverança, adequadas para todas as idades",
+trailer: "https://youtu.be/lJhvtAt_1Nk?feature=shared"
+}
+{
+title: "",A casa monstro (2006)
+genre: "",animação 
+ageRating: 10,
+synopsis: "",Três amigos decidem revelar a todos os perigos de uma casa vazia no bairro, que parece ter vida própria e devora pessoas!
+trailer: ""https://youtu.be/2bhP8oH1TJs?si=gAKce9rSkYMrGqrD
+}
+{
+title: "",Nimona (2023)
+genre: "",animação 
+ageRating: 12,
+synopsis: "",é a história de Ballister Boldheart, um cavaleiro acusado injustamente de um crime, que recebe ajuda de Nimona, uma jovem metamorfa rebelde e imprevisível. Juntos, eles formam uma parceria improvável para limpar seu nome, enfrentando uma sociedade que teme o desconhecido e desafiando preconceitos sobre o que é ser herói ou vilão
+trailer: ""https://youtu.be/f_fuHRyQbOc?si=pQHTt0XOZTwmFfWU
+}
+{
+title: "",Akira (1988)
+genre: "",animação 
+ageRating: 14,
+synopsis: "",Um dos animes mais icônicos da história, “Akira” se passa em um futuro distópico e aborda temas de poder, corrupção e controle. Com cenas de ação intensas e complexidade narrativa, é mais adequado para adolescentes mais velhos
+trailer: ""https://youtu.be/nA8KmHC2Z-g?si=Xz-dRhPWY_2nmQdf
+}
+{
+title: "",Entergalactic (2022)
+genre: "",animação 
+ageRating: 16,
+synopsis: "",segue Jabari, um jovem artista gráfico em Nova York, que se muda para um novo apartamento e conhece Meadow, sua vizinha fotógrafa. A história explora sua jornada de amor, autodescoberta e equilíbrio entre ambições profissionais e relacionamentos pessoais, tudo ambientado em uma cidade vibrante, com uma trilha sonora composta pelas músicas de Kid Cudi.
+trailer: ""https://youtu.be/YLbsoKX6Rfw?si=bTTuHBsGVXmVNUvH
+}
+{
+title: "",O Jardim Secreto (2020) 
+genre: "",:suspense 
+ageRating: L,
+synopsis: "Uma jovem órfã descobre um jardim misterioso e cheio de segredos
+",
+trailer: "https://youtu.be/XV5WmYjp1gw?feature=shared
+"
+}
+{
+title: ""Os Goonies (1985) ,
+genre: "",suspense 
+ageRating: 10,
+synopsis: "",Um grupo de amigos encontra um mapa do tesouro e embarca em uma aventura cheia de mistérios
+trailer: ""https://youtu.be/AaM2vMbY4_Y?si=xzEGLEPLcx1Fr6sT
+}
+{
+title: "",O Lar das Crianças Peculiares (2016) 
+genre: "",suspense 
+ageRating: 12,
+synopsis: "",Um jovem descobre uma casa cheia de crianças com poderes especiais e mistérios
+trailer: ""https://youtu.be/DNn2F2nIky8?si=BO3yF94xYeTLV168
+}
+{
+title: "",A Ilha do Medo (2010) 
+genre: "",suspense 
+ageRating: 14,
+synopsis: "",Dois detetives investigam um desaparecimento em um hospital psiquiátrico numa ilha isolada
+trailer: ""https://youtu.be/mtfgy46J260?si=HzW8nStXsNxRwsAm
+}
+{
+title: "",A Origem (2010) 
+genre: "",suspense 
+ageRating: 16,
+synopsis: "",Um suspense de ficção científica onde sonhos e realidade se misturam
+trailer: ""https://youtu.be/R_VX0e0PX90?si=lDgDCTMVyPzTbpFx
+}
+{
+title: "",Hotel Transilvânia (2012)
+genre: "",:terror
+ageRating: L,
+synopsis: "Um filme mais de comédia e aventura do que terror, mas com elementos de monstros que podem entreter os mais jovens
+",
+trailer: "https://youtu.be/WtyiO4ZpbmM?feature=shared
+"
+}
+{
+title: "",Coraline e o Mundo Secreto (2009) 
+genre: "",terror 
+ageRating: 10,
+synopsis: "",Coraline e o Mundo Secreto (2009) 
+sinopse: Embora seja mais um filme de animação e fantasia sombria, ele pode causar sustos leves. Indicado para crianças, mas com supervisão para os mais sensíveis
+trailer: ""https://youtu.be/X3Pc9emMSxk?feature=shared
+}
+{
+title: "",Ouija (2014)
+genre: "",terror
+ageRating: 12,
+synopsis: "",segue um grupo de amigos que, ao usar um tabuleiro Ouija para tentar se comunicar com uma amiga falecida, acabam libertando uma presença maligna que começa a aterrorizá-los. Eles descobrem segredos sombrios sobre o espírito e a casa, intensificando o terror sobrenatural.
+trailer: ""https://youtu.be/_T1Jj1inE8M?si=P3Rl1HMRSbvQ_JtD
+}
+{
+title: "",O Exorcista (1973)
+genre: "",terror
+ageRating: 14,
+synopsis: "", é um filme de terror clássico que segue a história de Regan, uma menina de 12 anos possuída por uma entidade demoníaca. Sua mãe, desesperada, busca a ajuda de dois padres para realizar um exorcismo. Conhecido por suas cenas chocantes e abordagem realista, o filme se tornou um marco do gênero de terror e é lembrado pelo impacto psicológico e pelo suspense intenso que provoca 
+trailer: ""https://youtu.be/7b__Mdcz_fc?si=0UuzbiA21Fp7V9ZW
+}
+{
+title: "",A Hora do Pesadelo (1984)
+genre: "",terror
+ageRating: 16,
+synopsis: "",é uma franquia de terror centrada em Freddy Krueger, um assassino que persegue e mata adolescentes em seus sonhos. Se eles morrem no sonho, morrem na vida real. A série explora o terror psicológico e a impotência de escapar de Freddy, já que ninguém pode evitar dormir
+trailer: ""https://youtu.be/HHNMjwYPlvY?si=38vadrDAE81SdrT1
+}
+{
+title: "",Wall-E (2008)
+genre: "",:Ficção científica
+ageRating: L,
+synopsis: "Animação da Pixar que acompanha um robô solitário em um futuro distópico que busca limpar a Terra enquanto encontra o amor. 
+",
+trailer: "https://youtu.be/m5_lIuBXKWk?feature=shared
+"
+}
+{
+title: "",Battleship: A Batalha dos Mares (2012) 
+genre: "",Ficção científica
+ageRating: 10,
+synopsis: "",segue uma frota naval que enfrenta uma invasão alienígena durante um exercício militar no Oceano Pacífico. O tenente Alex Hopper lidera a defesa contra a ameaça extraterrestre em intensas batalhas navais. Inspirado no jogo “Batalha Naval”,
+trailer: ""https://youtu.be/cp3646Zf8rg?si=TWHpM2mIMaV8ItIZ
+}
+{
+title: "",Depois da Terra (2013) 
+genre: "",Ficção científica
+ageRating: 12,
+synopsis: "",acompanha Kitai e seu pai, Cypher, que caem em uma Terra futurista e hostil após um acidente de nave. Com Cypher ferido, Kitai precisa atravessar o perigoso ambiente para encontrar ajuda. O filme explora temas de sobrevivência, coragem e a relação entre pai e filho
+trailer: ""https://youtu.be/u8uz0zjli28?si=KVmGmH4EF5feySA9
+}
+{
+title: "",Fim do mundo (2019)
+genre: "",Ficção científica
+ageRating: 14,
+synopsis: "",Depois de um ataque extraterrestre, a humanidade só tem uma esperança: um grupo de quatro adolescentes sem nada em comum isolados no seu acampamento de férias. 
+trailer: ""https://youtu.be/JM4kWeRaEfs?si=WyJhtQgaP9Z4vRbo
+}
+{
+title: "",O predador (2018)
+genre: "",Ficção científica
+ageRating: 16,
+synopsis: "",Quando a nave de um predador cai na terra, um rastreador de alienígenas, um órgão secreto do governo, um bióloga é um grupo de ex militares problemáticos precisam encontrá-la
+trailer:""https://youtu.be/69xfVCxIYXc?si=al8FIBbyBxvvGMVE
+}
+{
+title: "",A Era do Gelo (2002)
+genre: "",:Aventura 
+ageRating: L,
+synopsis: "conta a história de um grupo de animais improváveis – Manny, um mamute; Sid, uma preguiça; e Diego, um tigre-dentes-de-sabre – que se unem para devolver um bebê humano perdido à sua tribo durante a Era Glacial. Enquanto viajam, eles enfrentam desafios e perigos, e acabam desenvolvendo uma amizade inesperada. Paralelamente, Scrat, um esquilo-dentes-de-sabre obcecado por sua noz, tenta escondê-la, mas sempre acaba causando confusão.
+",
+trailer: "https://youtu.be/i4noiCRJRoE?feature=shared
+"
+}
+{
+title: "",Harry Potter e a Pedra Filosofal(2001)
+genre: "",Aventura
+ageRating: 10,
+synopsis: "",conta a história de Harry, um menino que descobre ser um bruxo e vai para a escola de magia de Hogwarts. Lá, ele faz amizade com Rony e Hermione e aprende sobre seu passado ligado ao bruxo das trevas Voldemort. Ao longo do ano, eles descobrem a existência da Pedra Filosofal, que Voldemort deseja para recuperar seu poder. Juntos, eles enfrentam desafios para proteger a pedra e evitar que ela caia nas mãos do inimigo.
+trailer: ""https://youtu.be/O7JVilOQdZ4?feature=shared
+}
+{
+title: "",Piratas do Caribe: A Maldição do Pérola Negra (2003)
+genre: "",Aventura
+ageRating: 12,
+synopsis: "",segue o Capitão Jack Sparrow e Will Turner em uma missão para resgatar Elizabeth Swann, sequestrada pelo pirata amaldiçoado Capitão Barbossa. Elizabeth possui um medalhão que pode quebrar a maldição do Pérola Negra, transformando os piratas em esqueletos sob a luz da lua. Jack e Will enfrentam perigos no mar, enquanto Jack busca vingança contra Barbosa.
+trailer: ""https://youtu.be/DGnatLS9Ohw?si=xiOCfkLVugrVmPMS
+}
+{
+title: "",Na Selva (2017)
+genre: "",Aventura
+ageRating: 14,
+synopsis: "",Baseado em uma história real, o filme acompanha Yossi Ginsberg (Daniel Radcliffe), que se perde na floresta amazônica após uma expedição dar errado. Ele luta contra os elementos e tenta sobreviver sozinho
+trailer: ""https://youtu.be/oJYJmYQ0AGw?si=R84yAJG6oU1Hs9p0
+}
+{
+title: "",Mad Max: Estrada da Fúria (2015)
+genre: "",Aventura
+ageRating: 16,
+synopsis: "",se passa em um futuro pós-apocalíptico onde Max Rockatansky é capturado por um tirano chamado Immortan Joe. Ele se une a Imperatriz Furiosa, que trai Joe para libertar um grupo de mulheres cativas. Juntos, eles enfrentam uma intensa perseguição pelo deserto, lutando pela liberdade.
+trailer: ""https://youtu.be/NH5TpcVaqCQ?si=yfQ8hTY6gPhcHzMw
+}
+{
